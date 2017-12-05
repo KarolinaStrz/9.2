06c7840a07c6a055cd017d2d450d72d9a3528d72
Karolina Strzelecka <kstrzelecka0594@gmail.com>
2017-12-05 18:14:17 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames)
+
+var newName = 'Marian'
+
+ if (  allNames.indexOf(newName) === -1)
+ {
+ 	var sprawdzone = allNames.push(newName)
+ }else {
+ 	condole.log("Podane imię już istnieje w tablicy")
+ }
+
+ console.log(allNames);
+
+var stringWithArray = allNames.toString();
+console.log(stringWithArray);  // "aaa, 2.55, bbb, 5"
